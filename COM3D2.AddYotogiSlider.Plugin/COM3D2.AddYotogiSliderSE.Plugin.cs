8d687216de027f1004c22479e67b00a63f2d529b
Vin-meido <86288831+Vin-meido@users.noreply.github.com>
2022-04-27 22:43:55 +0800

    Corrected version

--- COM3D2.AddYotogiSlider.Plugin/COM3D2.AddYotogiSliderSE.Plugin.cs
+++ COM3D2.AddYotogiSlider.Plugin/COM3D2.AddYotogiSliderSE.Plugin.cs
@@ -21,7 +21,7 @@ namespace COM3D2.AddYotogiSliderSE.Plugin
 {
     public static class VERSION
     {
-        public const string NUMBER = "1.0.0.10";
+        public const string NUMBER = "1.0.0.9";
 
 #if DEBUG
         public const string RELEASE_TYPE = "debug";
